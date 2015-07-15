893256557d42ee524e8f9e93914fba4d1baade0d
Sarthak Khillon <sarthakkhillon24@gmail.com>
2015-07-15 16:46:47 -0700

    Initial commit, up to label text being able to change.

---
+++ Crave App/View Controllers/PlateViewController.swift
@@ -0,0 +1,57 @@
+//
+//  PlateViewController.swift
+//  Crave App
+//
+//  Created by Pankaj Khillon on 7/15/15.
+//  Copyright (c) 2015 Sarthak Khillon. All rights reserved.
+//
+
+import UIKit
+
+class PlateViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subtitleLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MealCell", forIndexPath: indexPath) as! PlateTableViewCell
+        
+        cell.restaurantLabelHolder = "labl"
+        
+        let row = indexPath.row
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
