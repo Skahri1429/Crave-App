13c3271c3b7924f95ccd5fabd4cbdd71b51e4dd5
Sarthak Khillon <sarthakkhillon24@gmail.com>
2015-08-04 11:11:46 -0700

    STABLE BUILD, not sure yet if it works functionally.

--- Crave App/Models/UserChoiceCollectionDataSource.swift
+++ Crave App/Models/UserChoiceCollectionDataSource.swift
@@ -26,7 +26,7 @@ class UserChoiceCollectionDataSource {
     var categoryTagSearch: [String] = ["4bf58dd8d48988d1c1941735","4bf58dd8d48988d1c5941735"]
    
     let mealObject = MealObject()
-    let foundMeals: [MealObject] = []
+    var foundMeals: [MealObject] = []
     
     let currentUser = User()
     let ingredientData: [String]!
@@ -75,6 +75,7 @@ class UserChoiceCollectionDataSource {
             
            let tempSortedVenues = sortVenues(venueInformation)
            finishedVenueIdArray = filterVenues(tempSortedVenues)
+           findMeals(finishedVenueIdArray)
             
         }
         
@@ -164,108 +165,92 @@ class UserChoiceCollectionDataSource {
         }
         return filteredArray
     }
-//
-//    func findMeals(venueIDArray: [String]) { //REUSABLE IN CHOOSEVIEWCONTROLLER
-//        //this is where your TagData comes in. Use the filtered, sorted array to find meals for each venue element in array
-//        let venuesToSearch = venueIDArray
-//        let sortedMealObjects: [MealObject]
-//        
-//        for venue in venuesToSearch {
-//            
-//            let urlString = "https://api.foursquare.com/v2/venues/\(venue)/menu&client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=20150729"
-//            
-//            if let url = NSURL(string: urlString) { // if #1
-//                if let data = NSData(contentsOfURL: url, options: .allZeros, error: nil) { //if #2
-//                    let json = JSON(data: data)
-//                    if json["meta"]["code"].intValue == 200 { //if #3
-//                        if json["response"]["menu"]["menus"]["count"].int == 1 { //if #4
-//                            
-//                            for subcategories in json["response"]["menu"]["menus"]["items"]["entries"]["items"].arrayValue {
-//                                
-//                                for dishes in json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"].arrayValue {
-//                                
-//                                    mealObject.mealTitle = json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"]["name"].stringValue
-//                                    mealObject.mealDescription = json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"]["description"].stringValue
-//                                    mealObject.priceValue = json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"]["price"].doubleValue
-//                                    let urlString: String = json["response"]["menu"]["provider"]["attributionLink"].stringValue
-//                                    let urlArray: [String] = urlString.componentsSeparatedByString("/")
-//                                    mealObject.nameOfVenue = urlArray[3].capitalizedString
-//                                    foundMeals.append(mealObject)
-//                                    
-//                                }
-//                            }
-//                    
-//                        }// end if #4
-//                    
-//                } else {
-//                        println("Error in retrieving JSON")
-//                } // end else
-//                    
-//        }
-//      } //end if #1
-//            
-//            searchMealDescriptions(foundMeals)
-//            sortedMealObjects = sortMeals(foundMeals)
-//            // search Meal descriptions
-//            //sort meal descriptions
-//            // add number 1 to new array of top MealObjects
-//            
-//      } // end big for
-//    return sortedMealObjects
-//    }// end fuction
-//    
-//    func searchMealDescriptions(meals: [MealObject]) {
-//        
-//        let mealObjectArray = meals
-//        for mealItem in mealObjectArray {
-//            
-//            let mealDescription = mealItem.mealDescription // [String] of meal descriptions
-//            let characterSet: NSCharacterSet = NSCharacterSet.punctuationCharacterSet()
-//            let mealDescriptionWordsArray: [String] = (mealDescription.componentsSeparatedByCharactersInSet(characterSet) as NSArray).componentsJoinedByString("").componentsSeparatedByString(" ")
-//            
-//            var userFound: Double = 0
-//            let userBankArray = tagData.ingredientArray
-//            
-//            mealItem.score = calcScore(mealDescriptionWordsArray, userArray: userBankArray)
-//        }
-//    }
-//    
-//    func calcScore(wordArray: [String], userArray: [String]) -> Double {
-//        //let totalBank = bankArray
-//        let userBank = userArray
-//        let descriptionArray = wordArray
-//        for word in descriptionArray {
-//            for comparison in userBank {
-//                if word == comparison {
-//                    userFound++
-//                }
-//            }
-//        }
-//        let score = userFound / Double(userBank.count)
-//        return score
-//    }
-//    
-//    func sortMeals(meals: [MealObject]) -> [MealObject] { //sort by score
-//        var mealObjectArray = meals
-//        mealObjectArray.sort({ $0.score > $1.score })
-//        return mealObjectArray
-//    }
-//    
-//    func wordCount(toSearch: String, target: String) -> [String: Int] {
-//        var words = toSearch.componentsSeparatedByCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-//        var wordDictionary = [String: Int]()
-//        for word in words {
-//            if let count = wordDictionary[word] {
-//                wordDictionary[word] = count + 1
-//            } else {
-//                wordDictionary[word] = 1
-//            }
-//        }
-//        return wordDictionary
-//    }
-//    
-//    
-//    
+
+    func findMeals(venueIDArray: [String]) -> [MealObject] { //REUSABLE IN CHOOSEVIEWCONTROLLER
+        //this is where your TagData comes in. Use the filtered, sorted array to find meals for each venue element in array
+        let venuesToSearch = venueIDArray
+        var sortedMealObjects: [MealObject] = []
+        
+        for venue in venuesToSearch {
+            
+            let urlString = "https://api.foursquare.com/v2/venues/\(venue)/menu&client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=20150729"
+
+            if let url = NSURL(string: urlString) { // if #1
+                if let data = NSData(contentsOfURL: url, options: .allZeros, error: nil) { //if #2
+                    let json = JSON(data: data)
+                    if json["meta"]["code"].intValue == 200 { //if #3
+                        if json["response"]["menu"]["menus"]["count"].int == 1 { //if #4
+                            
+                            for subcategories in json["response"]["menu"]["menus"]["items"]["entries"]["items"].arrayValue {
+                                
+                                for dishes in json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"].arrayValue {
+                                
+                                    mealObject.mealTitle = json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"]["name"].stringValue
+                                    mealObject.mealDescription = json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"]["description"].stringValue
+                                    mealObject.priceValue = json["response"]["menu"]["menus"]["items"]["entries"]["items"]["entries"]["items"]["price"].doubleValue
+                                    let urlString: String = json["response"]["menu"]["provider"]["attributionLink"].stringValue
+                                    let urlArray: [String] = urlString.componentsSeparatedByString("/")
+                                    mealObject.nameOfVenue = urlArray[3].capitalizedString
+                                    foundMeals.append(mealObject)
+                                    
+                                }
+                            }
+                    
+                        }// end if #4
+                    
+                } else {
+                        println("Error in retrieving JSON")
+                } // end else
+                    
+        }
+      } //end if #1
+            
+            searchMealDescriptions(foundMeals)
+            sortedMealObjects = sortMeals(foundMeals)
+            // search Meal descriptions
+            //sort meal descriptions
+            // add number 1 to new array of top MealObjects
+            
+      } // end big for
+    return sortedMealObjects
+    }// end fuction
+
+    func searchMealDescriptions(meals: [MealObject]) {
+        
+        let mealObjectArray = meals
+        for mealItem in mealObjectArray {
+            
+            let mealDescription = mealItem.mealDescription // [String] of meal descriptions
+            let characterSet: NSCharacterSet = NSCharacterSet.punctuationCharacterSet()
+            let mealDescriptionWordsArray: [String] = (mealDescription.componentsSeparatedByCharactersInSet(characterSet) as NSArray).componentsJoinedByString("").componentsSeparatedByString(" ")
+            let userBankArray = tagData.ingredientArray
+            
+            mealItem.score = calcScore(mealDescriptionWordsArray, userArray: userBankArray)
+        }
+    }
+    
+    func calcScore(wordArray: [String], userArray: [String]) -> Double {
+        //let totalBank = bankArray
+        let userBank = userArray
+        var userFound: Double = 0
+        let descriptionArray = wordArray
+        for word in descriptionArray {
+            for comparison in userBank {
+                if word == comparison {
+                    userFound++
+                }
+            }
+        }
+        let score = userFound / Double(userBank.count)
+        return score
+    }
+    
+    func sortMeals(meals: [MealObject]) -> [MealObject] { //sort by score
+        var mealObjectArray = meals
+        mealObjectArray.sort({ $0.score > $1.score })
+        return mealObjectArray
+    }
+  
 }
 
 extension CLLocation {
