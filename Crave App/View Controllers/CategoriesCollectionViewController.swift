647c0687765c3eed71274f0e48751a255f02c19d
Sarthak Khillon <sarthakkhillon24@gmail.com>
2015-08-09 01:04:09 -0700

    Removed all unnecessary categoryIDs

--- Crave App/View Controllers/CategoriesCollectionViewController.swift
+++ Crave App/View Controllers/CategoriesCollectionViewController.swift
@@ -97,21 +97,21 @@ class CategoriesCollectionViewController: UICollectionViewController, UICollecti
                 categoriesSelected.append("503288ae91d4c4b30a586d67")
                 
                 case "African":
-                    let catHolder = "4bf58dd8d48988d1c8941735,4bf58dd8d48988d10a941735"
+                    let catHolder = "4bf58dd8d48988d1c8941735"
                     let catArray = catHolder.componentsSeparatedByString(",")
                     for cat in catArray {
                         categoriesSelected.append(cat)
                 }
                 
                 case "American":
-                    let catHolder = "4bf58dd8d48988d14e941735,4bf58dd8d48988d157941735,4bf58dd8d48988d1df931735,4bf58dd8d48988d16c941735,4d4ae6fc7a7b7dea34424761,4bf58dd8d48988d14c941735"
+                    let catHolder = "4bf58dd8d48988d14e941735"
                     let catArray = catHolder.componentsSeparatedByString(",")
                     for cat in catArray {
                         categoriesSelected.append(cat)
                     }
                 
                 case "Asian":
-                let catHolder = "4bf58dd8d48988d142941735,52e81612bcbc57f1066b7a03,4eb1bd1c3b7b55596b4a748f,52e81612bcbc57f1066b79fb,52af0bd33cf9994f4e043bdd,4bf58dd8d48988d111941735,55a59bace4b013909087cb0c,55a59bace4b013909087cb30,55a59bace4b013909087cb21,55a59bace4b013909087cb06,55a59bace4b013909087cb1b,55a59bace4b013909087cb1e,55a59bace4b013909087cb18,55a59bace4b013909087cb24,55a59bace4b013909087cb15,55a59bace4b013909087cb27,55a59bace4b013909087cb12,55a59bace4b013909087cb2d,55a59a31e4b013909087cb00,55a59af1e4b013909087cb03,55a59bace4b013909087cb2a,55a59bace4b013909087cb0f,55a59bace4b013909087cb33,55a59bace4b013909087cb09,55a59bace4b013909087cb36,4bf58dd8d48988d113941735,4bf58dd8d48988d156941735,4eb1d5724b900d56c88a45fe,4bf58dd8d48988d1d1941735,52af39fb3cf9994f4e043be9,4bf58dd8d48988d14a941735"
+                let catHolder = "4bf58dd8d48988d142941735"
                 let catArray = catHolder.componentsSeparatedByString(",")
                 for cat in catArray {
                 categoriesSelected.append(cat)
@@ -120,7 +120,7 @@ class CategoriesCollectionViewController: UICollectionViewController, UICollecti
                 case "Caribbean":
                 categoriesSelected.append("4bf58dd8d48988d144941735")
                 case "Chinese":
-                    let catHolder = "4bf58dd8d48988d145941735,52af3a5e3cf9994f4e043bea,52af3a723cf9994f4e043bec,52af3a7c3cf9994f4e043bed,52af3a673cf9994f4e043beb,52af3a903cf9994f4e043bee,4bf58dd8d48988d1f5931735,52af3a9f3cf9994f4e043bef,52af3aaa3cf9994f4e043bf0,52af3ab53cf9994f4e043bf1,52af3abe3cf9994f4e043bf2,52af3ac83cf9994f4e043bf3,52af3ad23cf9994f4e043bf4,52af3add3cf9994f4e043bf5,52af3af23cf9994f4e043bf7,52af3ae63cf9994f4e043bf6,52af3afc3cf9994f4e043bf8,52af3b053cf9994f4e043bf9,52af3b213cf9994f4e043bfa,52af3b293cf9994f4e043bfb,52af3b343cf9994f4e043bfc,52af3b3b3cf9994f4e043bfd,52af3b463cf9994f4e043bfe,52af3b633cf9994f4e043c01,52af3b513cf9994f4e043bff,52af3b593cf9994f4e043c00,52af3b6e3cf9994f4e043c02,52af3b773cf9994f4e043c03,52af3b813cf9994f4e043c04,52af3b893cf9994f4e043c05,52af3b913cf9994f4e043c06,52af3b9a3cf9994f4e043c07,52af3ba23cf9994f4e043c08"
+                    let catHolder = "4bf58dd8d48988d145941735"
                     let catArray = catHolder.componentsSeparatedByString(",")
                     for cat in catArray {
                         categoriesSelected.append(cat)
@@ -143,15 +143,15 @@ class CategoriesCollectionViewController: UICollectionViewController, UICollecti
             case "Hawaiian":
                 categoriesSelected.append("52e81612bcbc57f1066b79fe")
             case "Indian":
-                categoriesSelected.append("4bf58dd8d48988d10f941735,54135bf5e4b08f3d2429dfe5,54135bf5e4b08f3d2429dff3,54135bf5e4b08f3d2429dff5,54135bf5e4b08f3d2429dfe2,54135bf5e4b08f3d2429dff2,54135bf5e4b08f3d2429dfe1,54135bf5e4b08f3d2429dfe3,54135bf5e4b08f3d2429dfe8,54135bf5e4b08f3d2429dfe9,54135bf5e4b08f3d2429dfe6,54135bf5e4b08f3d2429dfdf,54135bf5e4b08f3d2429dfe4,54135bf5e4b08f3d2429dfe7,54135bf5e4b08f3d2429dfea,54135bf5e4b08f3d2429dfeb,54135bf5e4b08f3d2429dfed,54135bf5e4b08f3d2429dfee,54135bf5e4b08f3d2429dff4,54135bf5e4b08f3d2429dfe0,54135bf5e4b08f3d2429dfdd,54135bf5e4b08f3d2429dff6,54135bf5e4b08f3d2429dfef,54135bf5e4b08f3d2429dff0,54135bf5e4b08f3d2429dff1,54135bf5e4b08f3d2429dfde,54135bf5e4b08f3d2429dfec")
+                categoriesSelected.append("4bf58dd8d48988d10f941735")
             case "Indonesian":
-                categoriesSelected.append("52960eda3cf9994f4e043ac9,52960eda3cf9994f4e043acb,52960eda3cf9994f4e043aca,52960eda3cf9994f4e043acc,52960eda3cf9994f4e043ac7,52960eda3cf9994f4e043ac8,52960eda3cf9994f4e043ac5,52960eda3cf9994f4e043ac6")
+                categoriesSelected.append("52960eda3cf9994f4e043ac9")
             case "Italian":
-                categoriesSelected.append("4bf58dd8d48988d110941735,55a5a1ebe4b013909087cbb6,55a5a1ebe4b013909087cb7c,55a5a1ebe4b013909087cba7,55a5a1ebe4b013909087cba1,55a5a1ebe4b013909087cba4,55a5a1ebe4b013909087cb95,55a5a1ebe4b013909087cb89,55a5a1ebe4b013909087cb9b,55a5a1ebe4b013909087cb98,55a5a1ebe4b013909087cbbf,55a5a1ebe4b013909087cb79,55a5a1ebe4b013909087cbb0,55a5a1ebe4b013909087cbb3,55a5a1ebe4b013909087cb74,55a5a1ebe4b013909087cbaa,55a5a1ebe4b013909087cb83,55a5a1ebe4b013909087cb77,55a5a1ebe4b013909087cb8c,55a5a1ebe4b013909087cb92,55a5a1ebe4b013909087cb8f,55a5a1ebe4b013909087cb86,55a5a1ebe4b013909087cbb9,55a5a1ebe4b013909087cb7f,55a5a1ebe4b013909087cbbc,55a5a1ebe4b013909087cb9e,55a5a1ebe4b013909087cbc2,55a5a1ebe4b013909087cbad")
+                categoriesSelected.append("4bf58dd8d48988d110941735")
             case "Mediterranean":
                 categoriesSelected.append("4bf58dd8d48988d1c0941735,4bf58dd8d48988d1c3941735")
             case "Mexican":
-                categoriesSelected.append("4bf58dd8d48988d1c1941735,4bf58dd8d48988d153941735,4bf58dd8d48988d151941735")
+                categoriesSelected.append("4bf58dd8d48988d1c1941735")
             case "Persian":
                 categoriesSelected.append("52e81612bcbc57f1066b79f7")
             case "Pizza":
@@ -161,7 +161,7 @@ class CategoriesCollectionViewController: UICollectionViewController, UICollecti
             case "Steakhouse":
                     categoriesSelected.append("4bf58dd8d48988d1cc941735")
             case "Turkish":
-                categoriesSelected.append("4f04af1f2fb6e1c99f3db0bb,530faca9bcbc57f1066bc2f3,530faca9bcbc57f1066bc2f4,5283c7b4e4b094cb91ec88d8,5283c7b4e4b094cb91ec88d9,5283c7b4e4b094cb91ec88d7,5283c7b4e4b094cb91ec88db,5283c7b4e4b094cb91ec88d6,5283c7b4e4b094cb91ec88d5,5283c7b4e4b094cb91ec88da,530faca9bcbc57f1066bc2f2,5283c7b4e4b094cb91ec88d4")
+                categoriesSelected.append("4f04af1f2fb6e1c99f3db0bb")
             case "Thai":
             categoriesSelected.append("4bf58dd8d48988d149941735")
             default:
@@ -173,9 +173,4 @@ class CategoriesCollectionViewController: UICollectionViewController, UICollecti
         categoriesSelected.removeAtIndex(indexPath.row)
     }
 
-
 }
-
-//enum Category {
-//    case Afghan, African, American, Asian, Caribbean, Chinese, Deli, EastEuro, Falafel, French, German, Greek, Hawaiian, Indian, Indonesian, Italian, Mediterranean, Mexican, Persian, Pizza, Seafood, Steakhouse, Turkish, Thai
-//}
